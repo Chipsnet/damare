8afb911e4f7f6c079b64aa1417ba2e1110c3244b
Minato Minami <doronomori660@outlook.jp>
2021-08-02 04:32:22 +0900

    ✨ 完成した

--- index.js
+++ index.js
@@ -1,1 +1,123 @@
-console.log("log");
+const bunyan = require('bunyan');
+const { exec } = require('child_process');
+const Encoding = require('encoding-japanese');
+const packageJson = require('./package.json');
+const fs = require('fs');
+const { exit } = require('process');
+const Discord = require('discord.js');
+const chokidar = require('chokidar');
+const yaml = require("js-yaml");
+
+const log = bunyan.createLogger({name: 'damare', level: 'debug'});
+
+log.info("Damare reading bot v" + packageJson.version);
+
+log.info('Checking softalk...');
+
+if (fs.existsSync('./softalk/SofTalk.exe')) {
+    log.info('Softalk found.');
+} else {
+    log.error('Softalk not found. Can\'t Start damare. Please put softalk to current dir. If you want more info, visit https://github.com/Chipsnet/damare.');
+    exit()
+}
+
+try {
+    config = yaml.load(
+        fs.readFileSync("./config.yml", "utf-8")
+    );
+} catch (error) {
+    log.fatal('Config file not found. Please make config file. More information: https://github.com/Chipsnet/warbot-js.')
+    log.error(error);
+    process.exit(0)
+}
+
+const toString = (bytes) => {
+    return Encoding.convert(bytes, {
+      from: 'SJIS',
+      to: 'UNICODE',
+      type: 'string',
+    });
+};
+
+const client = new Discord.Client();
+const broadcast = client.voice.createBroadcast();
+let connection = null;
+let readMessages = [];
+let canReadMessage = true;
+
+client.on('ready', () => {
+    log.info('Discord login success! Logged in as : ' + client.user.tag);
+});
+
+client.on('message', async message => {
+    if (!message.guild) return;
+
+    if (message.content === ']talk') {
+        if (message.member.voice.channel) {
+            connection = await message.member.voice.channel.join();
+            connection.play(broadcast, {volume: 0.3});
+            message.reply('✨ VCに接続しました！');
+        }
+    }
+
+    if (message.content === ']stop') {
+        if (connection === null) {
+            message.reply('⚠ ボイスチャンネルに接続されていないので、切断ができませんでした。');
+        } else {
+            connection.disconnect();
+            message.reply('👍 無事切断できました')
+            connection = null;
+        }
+    }
+
+    if (message.channel.id === '464817274595835952' && message.content != ']talk' && message.author.bot == false) {
+        if (message.content.startsWith('http')) {
+            message.content = "ユーアールエル"
+        } 
+
+        if (canReadMessage) {
+            log.debug(`Message recived. canReadMessage: ${canReadMessage}`)
+            readMessages.push(message.content);
+            softalk();
+        } else {
+            log.debug(`Message recived. canReadMessage: ${canReadMessage}`)
+            readMessages.push(message.content);
+        }
+    }
+});
+
+async function softalk() {
+    canReadMessage = false;
+    let mes = readMessages.shift();
+
+    log.debug('softalk talk message: ' + mes);
+    log.debug('in queue' + readMessages);
+
+    exec('"./softalk/SofTalk.exe" /NM:女性01 /R:' + __dirname + '\\voice.wav /T:0 /X:1 /V:100 /W:' + mes, { encoding: 'Shift_JIS' }, (error, stdout, stderr) => {
+        if (error) {
+            log.error(toString(stderr));
+            if (readMessages.length === 0) {
+                canReadMessage = true;
+            } else {
+                softalk();
+            }
+            return;
+        }
+    })
+}
+
+chokidar.watch("./voice.wav").on('change', () => {
+    let dispatcher = broadcast.play('./voice.wav');
+
+    dispatcher.on('finish', () => {
+        if (readMessages.length === 0) {
+            canReadMessage = true;
+        } else {
+            softalk();
+        }
+    })
+})
+
+
+client.login(config.token);
+log.info('Trying Login to discord...');
